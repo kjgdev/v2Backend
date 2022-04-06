e60038604c588d117a91c940fbaf09d518b15577
MK <minhkhang.it123@gmail.com>
2022-04-06 20:52:43 +0700

    update new lib

--- src/controllers/settings/settings.database.js
+++ src/controllers/settings/settings.database.js
@@ -1,6 +1,6 @@
 const pool = require('@configs/database');
 const schedule = require('node-schedule');
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 const setTimeRunAgain = (day) => {
     let query = `UPDATE settings SET step = ? WHERE id = 1`
